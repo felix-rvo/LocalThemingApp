955d09ed09863ab818544ac091d5deac1f0e9754
jonathan <jfuentes@rvohealth.com>
2024-08-26 12:01:49 -0600

    Initial Commit

---
+++ LocalThemingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalThemingApp
+//
+//  Created by Jonathan Fuentes on 26/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
